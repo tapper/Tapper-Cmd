586ae463d8446dea444cb45f6d33548c9c05f169
Maik Hentsche <maik.hentsche@amd.com>
2010-08-18 09:02:04 +0200

    Cmd: [FEAT] validate preconditions
    Implemented a precondition checker using kwalify. The caller can provide
    a schema to check the preconditions against. Otherwise the default
    schema is used.

--- lib/Artemis/Cmd/Precondition.pm
+++ lib/Artemis/Cmd/Precondition.pm
@@ -4,6 +4,7 @@ use Moose;
 use Artemis::Model 'model';
 use Artemis::Exception::Param;
 use YAML::Syck;
+use Kwalify;
 
 use parent 'Artemis::Cmd';
 
@@ -26,6 +27,50 @@ testruns or preconditions in the database. This module handles the precondition 
 
 =cut
 
+=head2 die_on_invalid_precondition
+
+Check whether a precondition is valid either based on a given kwalify
+schema or the default schema. Errors are returned by die-ing.
+
+@param array ref - preconditions
+@param schema (optional)
+
+@return success 0
+
+@throws Perl die
+
+=cut
+
+sub die_on_invalid_precondition
+{
+        my ($self, $preconditions, $schema) = @_;
+        if (not ($schema and ref($schema) eq 'HASH') ) {
+                $schema = 
+                {
+                 type               => 'map',
+                 mapping            => 
+                 {
+                  precondition_type => 
+                  { type            => 'str',
+                    required        => 1,
+                  },
+                  '='               => 
+                  {
+                   type             => 'any',
+                   required         => 1,
+                  }
+                 }
+                };
+        }
+        $preconditions = [ $preconditions] unless ref($preconditions) eq 'ARRAY';
+ precondition:
+        foreach my $precondition (@$preconditions) {
+                # undefined preconditions are caused by artemis headers or a "---\n" line at the end
+                next precondition unless defined($precondition); 
+                Kwalify::validate($schema, $precondition);
+        }
+        return 0;
+}
 
 
 =head2 add
@@ -38,25 +83,25 @@ useful for macro preconditions.
 
 @param string    - preconditions in YAML format OR
 @param array ref - preconditions as list of hashes
-
+@param hash ref  - kwalify schema (optional)
 
 @return success - list of precondition ids
 @return error   - undef
 
-@throws Artemis::Exception::Param
+@throws Perl die
 
 =cut
 
 
 sub add {
-        my ($self, $input) = @_;
+        my ($self, $input, $schema) = @_;
         if (ref $input eq 'ARRAY') {
+                $self->die_on_invalid_precondition($input, $schema);
                 return model('TestrunDB')->resultset('Precondition')->add($input);
         } else {
                 $input .= "\n" unless $input =~ /\n$/;
-                my $yaml_error = Artemis::Schema::TestrunDB::_yaml_ok($input);
-                die Artemis::Exception::Param->new($yaml_error) if $yaml_error;
                 my @yaml = Load($input);
+                $self->die_on_invalid_precondition(\@yaml, $schema);
                 return model('TestrunDB')->resultset('Precondition')->add(\@yaml);
         }
 }
