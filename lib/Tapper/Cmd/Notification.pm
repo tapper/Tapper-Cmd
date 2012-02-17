d813ce145b1c0b6e454831c5d47f805ea7ecdeef
Maik Hentsche <maik.hentsche@amd.com>
2012-02-17 11:24:32 +0100

    Add listing of notifications
    This new functions returns a list of notification requests as DBIC
    resultset object. This way users of Cmd::Notification do not need to
    handle DBIC searches themselfes (i.e. don't need to use Tapper::Model).

---
+++ lib/Tapper/Cmd/Notification.pm
@@ -0,0 +1,185 @@
+package Tapper::Cmd::Notification;
+use Moose;
+
+use Tapper::Model 'model';
+use YAML::Syck;
+
+use parent 'Tapper::Cmd';
+
+=head1 NAME
+
+Tapper::Cmd::Notification - Backend functions for manipluation of notification subscriptions in the database
+
+=head1 SYNOPSIS
+
+This project offers backend functions for all projects that manipulate
+notification subscriptions.
+
+    use Tapper::Cmd::Notification;
+
+    my $subscription = Tapper::Cmd::Notification->new();
+
+    my $details = {event => "testrun_finished",
+                   check => "event.type == testrun_finish and testrun.id == 23",
+                   call  => 'jabber',
+                   comment => "Get back to work, testrun 23 is finished",
+                   persist => 0,
+                   owner   => 'anton',
+
+                  };
+    my $id = $subscription->add($details);
+    $details->{check} = "event.type == testrun_finish and testrun.id == 24";
+    my $error = $subscription->update($id, $details);
+    $error = $subscription->delete($id);
+
+=head1 FUNCTIONS
+
+=cut
+
+=head2 get_user
+
+Make sure the user is given as user id.
+
+@param hash ref - data for notification subscription
+
+@param success - updated hash ref
+
+@throws die
+
+=cut
+
+sub get_user
+{
+        my ($self, $data) = @_;
+        if (not $data->{user_id}) {
+                my $login = $data->{user_login} || $ENV{USER};
+                my $user = model('ReportsDB')->resultset('User')->search({login => $login})->first;
+                if (not $user) {
+                        die "User '$login' does not exist in the database. Please create this user first.\n";
+                }
+
+                $data->{user_id} = $user->id;
+                delete $data->{user_login};
+        }
+        return $data;
+}
+
+
+=head2 add
+
+Add a new notification subscription. Expects all details as a hash reference.
+
+
+@param string    - preconditions in YAML format OR
+@param hash ref  - notification subscrition data
+
+@return success - subscrition id
+@return error   - undef
+
+@throws Perl die
+
+=cut
+
+
+sub add {
+        my ($self, $data) = @_;
+
+        $data = $self->get_user($data);
+
+        my $notification = model('ReportsDB')->resultset('Notification')->new($data);
+        $notification->insert;
+
+        return $notification->id;
+}
+
+=head2 list
+
+Return a DBIC resultset object that contains a list of notification
+subscriptions.
+
+=cut
+
+sub list
+{
+        my ($self, $search) = @_;
+        return model('ReportsDB')->resultset('Notification')->search($search);
+}
+
+
+=head2 update
+
+Update a given notification subscription. The given data has to be a
+complete hash of what the subscription should look like after the
+update.
+
+@param int      - subscription id
+@param hash ref - subscription as it should be
+
+@return success - subscription id
+
+@throws die
+
+
+=cut
+
+sub update {
+        my ($self, $id, $data) = @_;
+
+        my $notification = model('ReportsDB')->resultset('Notification')->find($id);
+        die "Notification subscription with id $id not found\n" if not $notification;
+        die "Did not get a hash with data for updating notification subscription with id '$id'" unless ref $data eq 'HASH';
+
+        $data = $self->get_user($data);
+
+
+        foreach my $key (keys %$data) {
+                $notification->$key($data->{$key});
+        }
+        $notification->update;
+        return $notification->id;
+}
+
+
+=head2 del
+
+Delete a notification subscription with given id. Its named del instead of delete to
+prevent confusion with the buildin delete function.
+
+@param int - notification id
+
+@return success - 0
+
+@throws die
+
+=cut
+
+sub del {
+        my ($self, $id) = @_;
+        my $notification = model('ReportsDB')->resultset('Notification')->find($id);
+        die qq(No notification subscription with id "$id" found) if not $notification;;
+        $notification->delete();
+        return 0;
+}
+
+
+
+=head1 AUTHOR
+
+AMD OSRC Tapper Team, C<< <tapper at amd64.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<osrc-sysin at elbe.amd.com>, or through
+the web interface at L<https://osrc/bugs>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008-2011 AMD OSRC Tapper Team, all rights reserved.
+
+This program is released under the following license: freebsd
+
+=cut
+
+1; # End of Tapper::Cmd::Testrun
