f5f7a39664e15e9388ee555176f645a5612be433
Maik Hentsche <caldrin@amazon.com>
2013-04-18 13:05:31 -0700

    [FEAT] scenarios in testplans
    Scenarios are the way to have testruns that depend on each other
    (interdependend testruns), e.g. client-server tests. Yet, Tapper::Cmd
    support for scenarios was quite weak. the actual work was still done in
    the CLI and not moved into Cmd. This did not allow handling scenarios
    other than directly from the CLI.
    
    Now we want to have scenarios in testplans to create testplans with
    client-server tests. For this I moved the scenario work into Tapper::Cmd
    and called these functions for the testplan handler. This way testplans
    now recognise not only multitest but also scenario and thus interdep
    testruns.

--- lib/Tapper/Cmd/Scenario.pm
+++ lib/Tapper/Cmd/Scenario.pm
@@ -1,9 +1,15 @@
 package Tapper::Cmd::Scenario;
+
+use warnings;
+use strict;
+use 5.010;
 use Moose;
 
+use YAML::XS;
 use Tapper::Model 'model';
-
 use parent 'Tapper::Cmd';
+
+use Tapper::Cmd::Testrun;
 
 
 =head1 NAME
@@ -23,12 +29,80 @@ database on a higher level than that offered by Tapper::Schema.
 
 =head1 FUNCTIONS
 
+=head2 create
+
+Create a new scenario from one element of a test plan (actually a test
+plan instance). If the new scenario belong to a test plan instance the
+function expects the id of this instance as second parameter.
+
+@param hash ref - test plan element
+@param instance - test plan instance id
+
+@return array   - scenario ids
+
+=cut
+
+sub create {
+        my ($self, $scenario, $instance) = @_;
+        $scenario->{instance_id} ||= $instance;
+        return $self->add($scenario);
+}
+
+=head2 parse_interdep
+
+Parse an interdep scenario and do everything needed to put it into the
+database.
+
+@param hash ref - config containing all relevant information
+@param hash ref - options
+
+@return success - 0
+@return error   - die with error text
+
+=cut
+
+sub parse_interdep
+{
+        my ($self, $conf) = @_;
+
+        # TODO
+        # 1.) make atomic
+        # 2.) check whether more than 2 testruns are local and prevent sync in this case
+
+        my $scenario = model('TestrunDB')->resultset('Scenario')->new({type => 'interdep',
+                                                                       options => $conf->{scenario_options} || $conf->{options},
+                                                                       name => $conf->{scenario_name} || $conf->{name},
+                                                                      });
+        $scenario->insert;
+        my $sc_id    = $scenario->id;
+        my @all_ids;
+        foreach my $plan (@{$conf->{scenario_description} || $conf->{description}}) {
+                my $tr = Tapper::Cmd::Testrun->new();
+                $plan->{scenario_id} ||= $sc_id;
+                $plan->{status} = 'prepare';
+                my @ids = $tr->create($plan, $conf->{instance_id});
+                push @all_ids, @ids;
+        }
+
+        # when there is only on scenario element in the DB and the scheduler
+        # picks it it will think all scenario elements are fitted and start it
+        # therefore, we can only set them to 'schedule' when all testruns are in the DB
+        foreach my $id (@all_ids) {
+                my $testrun = model('TestrunDB')->resultset('Testrun')->find($id);
+                $testrun->testrun_scheduling->status('schedule');
+                $testrun->testrun_scheduling->update
+        }
+
+        return $sc_id;
+}
+
 
 =head2 add
 
-Add a new scenario to database
+Add a new scenario to database. Add with testplan instance id if given.
 
 @param hash ref - options for new scenario
+@param instance - test plan instance id
 
 @return success - scenario id
 @return error   - undef
@@ -36,11 +110,17 @@ Add a new scenario to database
 =cut
 
 sub add {
-        my ($self, $args) = @_;
-        my %args = %{$args};    # copy
-        my $scenario = model('TestrunDB')->resultset('Scenario')->new(\%args);
-        $scenario->insert;
-        return $scenario->id;
+        my ($self, $scenario) = @_;
+
+
+        given ($scenario->{scenario_type}) {
+                when ('interdep') {
+                        return $self->parse_interdep($scenario);
+                }
+                default {
+                        die "Unknown scenario type ", $scenario->{scenario_type};
+                }
+        }
 }
 
 
